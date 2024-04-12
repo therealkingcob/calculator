b9664254acdb116f59091001ab90a691b6e04e5d
therealkingcob <rishiashah19@gmail.com>
2024-04-11 19:18:02 -0700

    set up the display

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+let whichNumber = "first";
+document.addEventListener("DOMContentLoaded", function() {
 let operand1 = 0;
 let operand2 = 0;
 let operator = "";
+const number1 = document.getElementById("1");
+const number2 = document.getElementById("2");
+const number3 = document.getElementById("3");
+const number4 = document.getElementById("4");
+const number5 = document.getElementById("5");
+const number6 = document.getElementById("6");
+const number7 = document.getElementById("7");
+const number8 = document.getElementById("8");
+const number9 = document.getElementById("9");
+const display = document.getElementById("display");
+
 
 
 function operate (value1, operator, value2) {
     if (operator == "add") {
         addition(value1, value2);
     } else if (operator == "sub") {
-        subtraction(value1, Value2);
+        subtraction(value1, value2);
     } else if (operator == "mult") {
         multiplication(value1, value2);
     } else if (operator == "div") {
@@ -30,3 +43,15 @@ function multiplication () {
 function division () {
     return value1 / value2;
 }
+
+
+})
+
+function numberPressed(number) {
+    if (whichNumber == "first") {
+        display.innerHTML += number;
+        whichNumber = "second";
+    }
+}
+
+
