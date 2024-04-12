1fc5b15a11b62544f72ab7f446dadf7d96dbb708
therealkingcob <rishiashah19@gmail.com>
2024-04-11 20:30:23 -0700

    evaluate expressions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let whichNumber = "first";
-document.addEventListener("DOMContentLoaded", function() {
+let whichCharacter = "first";
+let answer = 0;
 let operand1 = 0;
 let operand2 = 0;
 let operator = "";
+document.addEventListener("DOMContentLoaded", function() {
+
 const number1 = document.getElementById("1");
 const number2 = document.getElementById("2");
 const number3 = document.getElementById("3");
@@ -15,43 +17,56 @@ const number9 = document.getElementById("9");
 const display = document.getElementById("display");
 
 
+})
+
+function characterPressed(type, character) {
+    if (whichCharacter == "first" && type == "number") {
+        display.innerHTML += character;
+        whichCharacter = "second";
+        operand1 = character;
+    } else if (whichCharacter == "second" && type == "operator") {
+        display.innerHTML += character;
+        whichCharacter = "third";
+        operator = character;
+    } else if (whichCharacter == "third" && type == "number") {
+        display.innerHTML += character;
+        whichCharacter = "fourth";
+        operand2 = character;
+    }
+}
+
+
+function equalKeyPressed() {
+    console.log(operator)
+    display.innerHTML = operate(operand1, operator, operand2);
+}
 
 function operate (value1, operator, value2) {
-    if (operator == "add") {
-        addition(value1, value2);
-    } else if (operator == "sub") {
-        subtraction(value1, value2);
-    } else if (operator == "mult") {
-        multiplication(value1, value2);
-    } else if (operator == "div") {
-        division(value1, value2);
+    if (operator == "+") {
+        return addition(value1, value2);
+        console.log(addition(value1, value2))
+    } else if (operator == "-") {
+        return subtraction(value1, value2);
+    } else if (operator == "*") {
+        return multiplication(value1, value2);
+    } else if (operator == "/") {
+        return division(value1, value2);
     }
 
 }
-function addition () {
+
+function addition (value1, value2) {
     return value1 + value2;
 }
 
-function subtraction () {
+function subtraction (value1, value2) {
     return value1 - value2;
 }
 
-function multiplication () {
+function multiplication (value1, value2) {
     return value1 * value2;
 }
 
-function division () {
+function division (value1, value2) {
     return value1 / value2;
 }
-
-
-})
-
-function numberPressed(number) {
-    if (whichNumber == "first") {
-        display.innerHTML += number;
-        whichNumber = "second";
-    }
-}
-
-
